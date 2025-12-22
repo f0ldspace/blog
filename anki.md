b55202e29ba9dcf58e6978970766c62faefe91a8
f0ld <me@f0ld.space>
2025-12-22 17:50:46 +0000

    anki setup

---
+++ anki.md
@@ -0,0 +1,11 @@
+---
+layout: default
+title: /anki
+permalink: /anki/
+---
+
+# Anki Stats
+
+Spaced repetition review statistics.
+
+- [2025](/anki/2025/)
--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     ruby_3_3
     bundler
+    sqlite
   ];
 }
